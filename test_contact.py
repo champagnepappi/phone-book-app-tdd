a90fbc96544c6b6d5f7951ba2f4ec34315306fc9
kevin <kevinochieng548@gmail.com>
2018-03-01 14:56:05 +0300

    [#155599318] define the setup method

--- test_contact.py
+++ test_contact.py
@@ -2,30 +2,28 @@ import unittest
 from contact import Contact
 
 class PhonebookTestCase(unittest.TestCase):
+    def setUp(self):
+        self.contact = Contact()
+
     def test_add_contact(self):
-        contact = Contact()
-        result = contact.add_contact("pete", "0712351423")
+        result = self.contact.add_contact("pete", "0712351423")
         self.assertEqual(result["info"], "Contact added")
 
     def test_add_contact_with_contact_greater_than_10_digits(self):
-        contact = Contact()
-        result = contact.add_contact("anne", "3414444444444414")
+        result = self.contact.add_contact("anne", "3414444444444414")
         self.assertNotEqual(result["info"], "Contact added")
         self.assertEqual(result["info"], "Re-enter the contact, contact must be 10 digits")
 
     def test_delete_contact(self):
-        contact = Contact()
-        result = contact.delete_contact("jane", "1873137103")
+        result = self.contact.delete_contact("jane", "1873137103")
         self.assertEqual(result["info"], "Contact deleted")
 
     def test_update_contact(self):
-        contact = Contact()
-        result = contact.update_contact("jane", "34251617181", "5463727828")
+        result = self.contact.update_contact("jane", "34251617181", "5463727828")
         self.assertEqual(result, "My new contact is 5463727828")
 
     def test_view_contacts(self):
-        contact = Contact()
-        result = contact.view_contacts("shaz", "2151853811")
+        result = self.contact.view_contacts("shaz", "2151853811")
         self.assertEqual(result, "Contact for shaz is 2151853811")
 
 
